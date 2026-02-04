687ce4a26862cf0af8f86855eb36ca41fb5f1246
袁皓宇 <3403704698@qq.com>
2026-02-04 22:05:08 +0800

    +角色增删改查后端

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, re_path
 
 from . import views
+from .views.create.character.create import CreateCharacterView
+from .views.create.character.get_single import GetSingleCharacterView
+from .views.create.character.remove import RemoveCharacterView
+from .views.create.character.update import UpdateCharacterView
 from .views.index import index
 from .views.user.account.get_user_info import GetUserInfoView
 from .views.user.account.login import LoginView
@@ -10,12 +14,22 @@ from .views.user.account.register import RegisterView
 from .views.user.profile.update import UpdateProfileView
 
 urlpatterns = [
+    # 用户
     path('api/user/account/login/', LoginView.as_view()),
     path('api/user/account/logout/', LogoutView.as_view()),
     path('api/user/account/register/', RegisterView.as_view()),
     path('api/user/account/refresh_token/', RefreshTokenView.as_view()),
     path('api/user/account/get_user_info/', GetUserInfoView.as_view()),
+
+    # 编辑个人信息
     path('api/user/profile/update/', UpdateProfileView.as_view()),
+
+    # 创作
+    path('api/create/character/create/', CreateCharacterView.as_view()),
+    path('api/create/character/update/', UpdateCharacterView.as_view()),
+    path('api/create/character/remove/', RemoveCharacterView.as_view()),
+    path('api/create/character/get_single/', GetSingleCharacterView.as_view()),
+
     # 前端页面路由
     path('', index),
     re_path(r'^(?!media/|static/|assets/).*$', index),

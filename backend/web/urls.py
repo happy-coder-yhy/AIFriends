de870dc0bdf254dfe32fb7d0453219b383045acd
袁皓宇 <3403704698@qq.com>
2026-02-04 09:48:19 +0800

    +更新用户资料后端

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -7,6 +7,7 @@ from .views.user.account.login import LoginView
 from .views.user.account.logout import LogoutView
 from .views.user.account.refresh_token import RefreshTokenView
 from .views.user.account.register import RegisterView
+from .views.user.profile.update import UpdateProfileView
 
 urlpatterns = [
     path('api/user/account/login/', LoginView.as_view()),
@@ -14,6 +15,7 @@ urlpatterns = [
     path('api/user/account/register/', RegisterView.as_view()),
     path('api/user/account/refresh_token/', RefreshTokenView.as_view()),
     path('api/user/account/get_user_info/', GetUserInfoView.as_view()),
+    path('api/user/profile/update/', UpdateProfileView.as_view()),
     # 前端页面路由
     path('', index),
     re_path(r'^(?!media/|static/|assets/).*$', index),

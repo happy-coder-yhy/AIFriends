6d43b75e3220ff3ece82c26da4f105c301588f3a
袁皓宇 <3403704698@qq.com>
2026-02-06 20:09:21 +0800

    +更新角色前端

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -7,6 +7,7 @@ import LoginIndex from '@/views/user/account/LoginIndex.vue';
 import RegisterIndex from '@/views/user/account/RegisterIndex.vue';
 import SpaceIndex from '@/views/user/space/SpaceIndex.vue';
 import ProfileIndex from '@/views/user/profile/ProfileIndex.vue';
+import UpdateCharacter from '@/views/create/character/UpdateCharacter.vue';
 import { useUserStore } from '@/stores/user';
 
 const router = createRouter({
@@ -32,6 +33,14 @@ const router = createRouter({
       path: '/create',
       component: CreateIndex,
       name: 'create-index',
+      meta: {
+        needLogin: true,
+      }
+    },
+    {
+      path: '/create/character/update/:character_id',
+      component: UpdateCharacter,
+      name: 'update-character',
       meta: {
         needLogin: true,
       }
